63b282a9e3eca44860973684dcca2631bfab0a89
Henrique Massareli <henrique@oneseventech.com>
2023-05-13 14:52:41 -0300

    feat: handle post transactions

--- src/app/uploads/page.tsx
+++ src/app/uploads/page.tsx
@@ -1,15 +1,29 @@
 "use client";
 import { postTransactions } from "@/services";
 import { Transaction } from "@/types";
-import { useContext } from "react";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
 import { TransactionsContext } from "../../../context/context";
 
 const UploadsPage = () => {
+  const router = useRouter();
   const { transactions } = useContext(TransactionsContext);
+
+  const handlePostTransactions = async () => {
+    await postTransactions(transactions);
+    router.push("/users");
+  };
+
+  useEffect(() => {
+    if (transactions.length === 0) {
+      router.push("/");
+    }
+  }, [router, transactions]);
 
   return (
     <main className="flex min-h-screen flex-col items-center ">
-      <div className=" h-full mt-7 max-h-[700px] overflow-auto scroll w-full max-w-4xl rounded-b-lg">
+      <h1 className="text-2xl font-bold mt-5"> Check your Uploads</h1>
+      <div className=" h-full mt-7 max-h-[600px] overflow-auto scroll w-full max-w-4xl rounded-b-lg">
         <div className="flex flex-col items-center justify-center gap-4 w-full">
           <div className="flex gap-2 px-8 rounded-t-lg w-full justify-between font-bold bg-[#1944A0] text-white py-3">
             <p className="w-[200px] ">Seller</p>
@@ -37,12 +51,10 @@ const UploadsPage = () => {
         </div>
       </div>
       <p
-        onClick={() => {
-          postTransactions(transactions);
-        }}
-        className="border mt-6 border-green-500 cursor-pointer rounded-lg text-green-500 font-semibold bg-[#4dc76d2c] p-4"
+        onClick={handlePostTransactions}
+        className="border mt-6 cursor-pointer rounded-lg text-white font-medium bg-[#158582] p-4"
       >
-        Send your transactions
+        Send transactions
       </p>
     </main>
   );
