2291a433f97afbc45cfa2ae5322510641352a1fe
Henrique Massareli <henrique@oneseventech.com>
2023-05-15 13:18:47 -0300

    feat: add navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { TransactionsProvider } from "../../context/context";
 import "./globals.css";
 
@@ -14,10 +17,45 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathName = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransactionsProvider>{children}</TransactionsProvider>
+        <TransactionsProvider>
+          <div className="flex justify-evenly font-semibold items-center rounded-lg w-[400px] h-11 m-auto mt-5 gap-3 text-[#1944A0]">
+            <Link
+              className={
+                pathName === "/"
+                  ? "underline underline-offset-4 decoration-2"
+                  : ""
+              }
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={
+                pathName === "/transactions"
+                  ? "underline underline-offset-4 decoration-2"
+                  : ""
+              }
+              href="/transactions"
+            >
+              Transactions
+            </Link>
+            <Link
+              className={
+                pathName === "/users"
+                  ? "underline underline-offset-4 decoration-2"
+                  : ""
+              }
+              href="/users"
+            >
+              Users
+            </Link>
+          </div>
+          {children}
+        </TransactionsProvider>
       </body>
     </html>
   );
