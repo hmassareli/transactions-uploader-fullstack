1bfb02feadb2cff023e4449409e3fb3449115169
Henrique Massareli <henrique@oneseventech.com>
2023-05-16 15:54:08 -0300

    feat: add date to the transactions history

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,5 +1,6 @@
 import { Transaction } from "@/types";
 import { fromCentsToDollars } from "@/utils";
+import moment from "moment";
 
 const Transactions = ({
   allTransactions,
@@ -10,6 +11,7 @@ const Transactions = ({
     <div className=" h-full mt-7 max-h-[600px] overflow-auto scroll w-full max-w-4xl rounded-b-lg">
       <div className="flex flex-col items-center justify-center gap-4 w-full">
         <div className="flex gap-2 px-8 rounded-t-lg w-full justify-between font-bold bg-[#1944A0] text-white py-3">
+          <p className="w-[200px] ">Date</p>
           <p className="w-[200px] ">Seller</p>
           <p className="w-[250px] ">Product</p>
           <p className="w-[100px] ">Amount</p>
@@ -23,6 +25,10 @@ const Transactions = ({
             className="odd:bg-[#b7bbdd3a] flex py-3 gap-2 px-8 w-full justify-between font-bold text-sm"
             key={index}
           >
+            <p className="w-[200px] text-[#1944A0]">
+              {" "}
+              {moment(transaction.date).format("YYYY-MM-DD")}
+            </p>
             <p className="w-[200px] text-[#1944A0]">{transaction.sellerName}</p>
             <p className="w-[250px] text-[#1944A0]">
               {transaction.productName}
